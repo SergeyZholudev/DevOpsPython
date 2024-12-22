e1bfd5fe7e0f45c90db17e4d96e2160a821f9c8d
SergeyZholudev <user@example.com>
2024-12-22 22:42:24 +0300

    dz3 & dz2-3BONUS_task_2 are completed

--- dz2-3BONUS_task_2.py
+++ dz2-3BONUS_task_2.py
@@ -2,3 +2,43 @@
 # •	Количество положительных чисел.
 # •	Произведение всех отрицательных чисел.
 # •	Минимальное и максимальное числа без использования функций min() и max().
+
+while True:
+    try:
+        inputString = input('Введите последовательность чисел через пробел: ')
+        numbers = list(map(int, inputString.split(' ')))  # преобразуем всю введенную последовательность в список чисел
+        break
+    except ValueError:
+        print('Все элементы списка должны быть целыми числами. Повторите ввод.')
+
+# сортировка списка
+numbersPositive = []
+numbersNegative = []
+for num in numbers:
+    if num > 0:
+        numbersPositive.append(num)
+    elif num < 0:
+        numbersNegative.append(num)
+
+# Произведение всех отрицательных чисел.
+multiplNegative = 1
+if len(numbersNegative) != 0:
+    for j in numbersNegative:
+        multiplNegative *= j
+else:
+    multiplNegative = len(numbersNegative)
+
+# Минимальное и максимальное числа без использования функций min() и max()
+maxNumber = numbers[0]
+minNumber = numbers[0]
+for num in numbers:
+    if num > maxNumber:
+        maxNumber = num
+    elif num < minNumber:
+        minNumber = num
+
+# блок вывода результатов
+print(f'Количество положительных чисел = {len(numbersPositive)}')
+print(f'Произведение всех отрицательных чисел = {multiplNegative}')
+print(f'Максимальное число в списке: {maxNumber}')
+print(f'Минимальное число в списке: {minNumber}')
---
+++ dz3.py
@@ -0,0 +1,10 @@
+# Написать программу, которая выводит в консоль таблицу Escape-последовательностей:
+
+print('Escape sequences')
+print('\\a' + ' ' *6 + 'Bell(alert)')
+print('\\b' + ' ' *6 + 'Backspace')
+print('\\n' + ' ' *6 + 'New line')
+print('\\t' + ' ' *6 + 'Horizontal tab')
+print(r'\\' + ' ' *6 + 'Backslash \\')
+print('\\"' + ' ' *6 + 'Double quotation mark "')
+print(r'\'' + ' ' *6 + 'Double quotation mark \'')
